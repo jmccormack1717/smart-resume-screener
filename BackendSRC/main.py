3881ff37a644c3197e734b96f0c8199ff352069c
jmccormack1717 <118073033+jmccormack1717@users.noreply.github.com>
2025-05-24 15:01:56 +0000

    Updated CORS settings

--- BackendSRC/main.py
+++ BackendSRC/main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 # Allow frontend (Amplify) to talk to backend (adjust origin accordingly)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # For testing only, restrict in production
+    allow_origins=["https://frontend.d3qbsni8smwyay.amplifyapp.com/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
