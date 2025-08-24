e39463f79784aca82c7c54de1436b132422e7541
jmccormack1717 <118073033+jmccormack1717@users.noreply.github.com>
2025-08-24 00:18:59 +0000

    s

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,7 @@
 // src/App.jsx
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "my-app/src/pages/Home.jsx";
-import Results from "my-app/src/pages/Results.jsx";
+import Home from "/workspaces/smart-resume-screener/my-app/src/pages/Home.jsx";
+import Results from "/workspaces/smart-resume-screener/my-app/src/pages/Results.jsx";
 
 export default function App() {
   return (
