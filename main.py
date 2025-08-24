98e91bb7fc56d429b2a01034bba657d6abf15a71
jmccormack1717 <118073033+jmccormack1717@users.noreply.github.com>
2025-08-24 02:46:13 +0000

    s

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ app = FastAPI()
 # Allow frontend (React dev server on localhost:3000) to talk to backend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"]
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
